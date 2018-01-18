502a5ae801ec7fcbeae4d682f8173f82f6c0b677
shantan94 <shantanreddy94@gmail.com>
2018-01-17 20:53:24 -0600

    forced https redirect

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
 const app = express();
+app.use(sslRedirect());
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
